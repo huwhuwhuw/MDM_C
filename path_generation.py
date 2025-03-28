c242db9a695ccb8c66b5720f0cb35f3091e9bf97
Kate Holloway <xw20103@bristol.ac.uk>
2025-03-28 11:41:21 +0000

    fixed path generation file handling bug

--- path_generation.py
+++ path_generation.py
@@ -119,23 +119,26 @@ class Data_Management:
 
         return self.coords
 
-    def load_OA_clusters(self, filename='markov_oa_codes_merged.csv'):
+    def load_OA_clusters(self, filename):
         """ loads clusters from file """
+        # filename : name of csv file containing clusters
         # self.clusters = pd.read_csv(os.path.join(self.csv_folder, filename))
         # # cluster locations are in both home and work columns (weirdly) and contain duplicates
         # self.clusters = list(pd.concat(
         #     [self.clusters['Home Cluster OA Code'], self.clusters['Work Cluster OA Code']]).unique())
 
         self.clusters = pd.read_csv(os.path.join(self.csv_folder, filename))
-        self.clusters = self.clusters['OA_code'].unique()
+        # self.clusters = list(self.clusters['OA_Code'])
+        self.clusters = self.clusters['OA_Code'].unique()
 
         return self.clusters
 
-    def load_cluster_coords(self, filename='msoa_lookup.csv'):
+    def load_cluster_coords(self, clusterfile, filename='msoa_lookup.csv'):
         """ loads coordinates for clusters """
-        self.load_OA_clusters()
+        self.clusters = self.load_OA_clusters(clusterfile)
         self.coords = pd.read_csv(os.path.join(self.csv_folder, filename))
-        self.coords = self.coords[self.coords['OA_code'].isin(self.clusters)]
+
+        self.coords = self.coords[self.coords['OA_Code'].isin(self.clusters)]
         return self.coords
 
     def load_route(self, start, end):
@@ -161,12 +164,12 @@ class Data_Management:
 
         return self.route
 
-    def dynamic_radius(self, k=5, alpha=3.5):
+    def dynamic_radius(self, k=5, alpha=3.5, filename='markov_oa_codes_merged.csv'):
         """ implements a dynamic radius to generate graph from
             nodes with closer neighbours have a lower maximum connection radius """
 
         # create copy of coordinates to convert to lat/long without impacting other functions
-        coordinates = self.load_cluster_coords()
+        coordinates = self.load_cluster_coords(clusterfile=filename)
 
         # radius has to be stored with coords to allow oa code indexing
         coordinates['radius'] = np.nan
@@ -183,34 +186,32 @@ class Data_Management:
 
         for cluster in self.clusters:
             # get distance of k nearest nodes from every node
-            self.distance, _ = self.tree.query([self.coords.loc[self.coords['OA_code'] == cluster, 'Longitude'].iloc[0],
-                                                self.coords.loc[self.coords['OA_code'] == cluster, 'Latitude'].iloc[0]],
+            self.distance, _ = self.tree.query([self.coords.loc[self.coords['OA_Code'] == cluster, 'Longitude'].iloc[0],
+                                                self.coords.loc[self.coords['OA_Code'] == cluster, 'Latitude'].iloc[0]],
                                                k=k)
 
-            coordinates.loc[coordinates['OA_code'] == cluster,
+            coordinates.loc[coordinates['OA_Code'] == cluster,
                             'radius'] = np.mean(self.distance) * alpha
 
         return coordinates
 
-    def generate_graph(self, clusterfile=None, k=5, alpha=4):
+    def generate_graph(self, clusterfile, k=5, alpha=4):
         """ generate adjacency matrix of routes between clusters
             max_distance : int, maximum distance of routes between clusters in meters"""
-        if clusterfile:
-            self.load_OA_clusters(clusterfile)
-        elif not clusterfile:
-            self.load_OA_clusters()
+
+        self.load_OA_clusters(clusterfile)
 
         # create maximum radius for each node
-        coords = self.dynamic_radius(k, alpha)
+        coords = self.dynamic_radius(k, alpha, clusterfile)
 
         # adjaceny matrix for graph
         self.adj = pd.DataFrame(index=self.clusters, columns=self.clusters)
-        # iterate through every combination of
+        # iterate through every combination of clusters
         for clusterA in self.clusters:
             for clusterB in self.clusters:
                 try:
                     self.load_route(clusterA, clusterB)
-                    if self.route['distance'] < coords.loc[coords['OA_code'] == clusterA, 'radius'].iloc[0]:
+                    if self.route['distance'] < coords.loc[coords['OA_Code'] == clusterA, 'radius'].iloc[0]:
                         self.adj.loc[clusterA,
                                      clusterB] = self.route['distance']
                 except FileNotFoundError:
@@ -221,11 +222,11 @@ class Data_Management:
 
 
 class OA_Plot:
-    def __init__(self, handler):
+    def __init__(self, handler, clusterfile):
         self.fig, self.ax = plt.subplots(figsize=(6, 6))
 
         # import clustered OA codes and convert them to a geopandas dataframe
-        self.clusters = handler.load_cluster_coords()
+        self.clusters = handler.load_cluster_coords(clusterfile)
         self.clusters = {'geometry': [Point(long, lat) for lat, long in zip(
             self.clusters['Latitude'], self.clusters['Longitude'])]}
         self.clusters = gpd.GeoDataFrame(self.clusters, crs='EPSG:4326')
@@ -263,8 +264,8 @@ def Plot(k, alpha, clusterfile='markov_oa_codes_merged.csv'):
     # load clusters and initialise plot
     handler = Data_Management()
     handler.load_OA_clusters(clusterfile)
-    coords = handler.dynamic_radius(k, alpha)
-    figure = OA_Plot(handler)
+    radius_coords = handler.dynamic_radius(k, alpha, filename=clusterfile)
+    figure = OA_Plot(handler, clusterfile)
 
     max_distance = 2000  # maximum distance to connect cluster by (meters)
 
@@ -273,12 +274,13 @@ def Plot(k, alpha, clusterfile='markov_oa_codes_merged.csv'):
         for clusterB in handler.clusters:
             try:
                 handler.load_route(clusterA, clusterB)
+                if handler.route['distance'] < radius_coords.loc[handler.coords['OA_Code'] == clusterA, 'radius'].iloc[0]:
+                    figure.create_route_plot(handler.route)
+
             except FileNotFoundError:
                 # some routes are only calculated one way, ignore attempts to load that route reversed
                 # or if using a different cluster set some routes may no be calculated
                 print(f'Route between {clusterA} and {clusterB} not found')
-            if handler.route['distance'] < coords.loc[handler.coords['OA_code'] == clusterA, 'radius'].iloc[0]:
-                figure.create_route_plot(handler.route)
 
     # add pyplot parameters like color, size, etc to create_scatter_plot as usual
     figure.create_scatter_plot()
@@ -288,11 +290,11 @@ def Plot(k, alpha, clusterfile='markov_oa_codes_merged.csv'):
     figure.fig.show()
 
 
-def main(k, alpha, count, clusterfile='markov_oa_code_merged.csv'):
+def main(k, alpha, count, clusterfile='markov_oa_codes_merged.csv'):
     # generate the directional graph
 
     handler = Data_Management()
-    adjacency_matrix = handler.generate_graph(k=k, alpha=alpha)
+    adjacency_matrix = handler.generate_graph(clusterfile, k=k, alpha=alpha)
     adjacency_matrix.fillna(0, inplace=True)
     network = nx.from_pandas_adjacency(
         adjacency_matrix, create_using=nx.DiGraph)
@@ -300,13 +302,12 @@ def main(k, alpha, count, clusterfile='markov_oa_code_merged.csv'):
     # calculate most central betweenness node
     between_center = nx.betweenness_centrality(network)
 
-    handler.load_cluster_coords()
-    between = handler.coords.loc[handler.coords['OA_code'] == max(
+    handler.load_cluster_coords(clusterfile)
+    between = handler.coords.loc[handler.coords['OA_Code'] == max(
         between_center, key=between_center.get)]
 
     routes = []
-    center = between['OA_code'].iloc[0]
-    print(center)
+    center = 'E00174312'  # k=40 cluster center(ish) node
     colors = ['red', 'green', 'blue', 'gold', 'purple', 'black']
 
     # iteratively create routes removing already visited nodes each time
@@ -332,19 +333,27 @@ def main(k, alpha, count, clusterfile='markov_oa_code_merged.csv'):
         for route in potential_routes:
             line_scores.append(Indivual_Line_Score.Line_Score(route, coords))
 
-        # add the route nodes and total score to the record
-        routes.append(
-            [potential_routes[line_scores.index(max(line_scores))], max(line_scores)])
+        try:
+            # add the route nodes and total score to the record
+            routes.append(
+                [potential_routes[line_scores.index(max(line_scores))], max(line_scores)])
+        except:
+            print(f'No routes found')
+            break
 
         # remove the visited route from the graph while keeping the center
         print(routes)
         nodes_to_drop = routes[-1][0].copy()
-        nodes_to_drop.remove(center)
-        print(nodes_to_drop)
-        network.remove_nodes_from(nodes_to_drop)
+        # nodes_to_drop.remove(center)
+        print(nodes_to_drop[-1])
+        try:
+            network.remove_nodes_from(nodes_to_drop[-3:])
+        except:
+            print(f'Route length too short')
+            break
 
     # plotting only the best routes
-    figure = OA_Plot(handler)
+    figure = OA_Plot(handler, clusterfile)
 
     for i, route in enumerate(routes):
         for cluster_index in range(0, len(route[0])-1):
@@ -355,6 +364,7 @@ def main(k, alpha, count, clusterfile='markov_oa_code_merged.csv'):
             figure.create_route_plot(
                 segment, color=colors[i], label=f'Route {i}')
 
+    figure.ax.legend()
     figure.create_scatter_plot()
     figure.add_basemap()
 
@@ -370,10 +380,13 @@ def import_json():
 
 
 if __name__ == '__main__':
-    k = 5
-    alpha = 3
-    count = 4
-    Plot(k, alpha)
-    main(k, alpha, count)
+    k = 4
+    alpha = 3.5
+    count = 6   # maximum 6
+    # filename of clusters; 40, 50, 60, markov
+    clusterfile = 'kmedoids_oa_codes_60.csv'
+    Plot(k, alpha, clusterfile)     # plots full graph of network
+    # calculates and plots top {count} lines
+    # main(k, alpha, count, clusterfile)
     # Route_Cleaner('routeA')
     # Pickle_Test()
